c2e4dd903d733ac0837ddc904745f87028bc62f6
bai <nkbai@163.com>
2019-01-31 17:32:02 +0800

    webdemo: update ui

--- cmd/webdemo/dnotary-webdemo/app/test.js
+++ cmd/webdemo/dnotary-webdemo/app/test.js
@@ -455,11 +455,13 @@ function queryStatus(obj) {
         $("#SideChainBlockNumber").html(r.SideChainBlockNumber)
 
         $("#mainChainContractBalance").html('<a target="_blank" href="https://ropsten.etherscan.io/address/'+mainChainContract+'">'+  getMainChainWeb3().fromWei(r.MainChainContractBalance, "ether")+" Ether</a>")   //(getMainChainWeb3().fromWei(r.MainChainContractBalance, "ether"))
-        $("#sideChainContractBalance").html(getMainChainWeb3().fromWei(r.SideChainContractBalance, "ether"))
+        $("#sideChainContractBalance").html('<a target="_blank" href="https://chain.smartmesh.io/token.html?source=commonts&tokenF='+myaccount+'">'+getMainChainWeb3().fromWei(r.SideChainContractBalance, "ether") +' EtherToken</a>')
+        //https://chain.smartmesh.io/token.html?source=commonts&tokenF=0x4931ada8fc103e81dbeb35ed04abf58686c1a9b4
         if(myaccount) {
             $("#MainChainBalance").html('<a  target="_blank" href="https://ropsten.etherscan.io/address/'+myaccount+'">'+  getMainChainWeb3().fromWei(r.MainChainBalance, "ether")+" Ether</a>")
             $("#SideChainBalance").html ('<a target="_blank" href="https://chain.smartmesh.io/address.html?address='+myaccount+'">'+getMainChainWeb3().fromWei(r.SideChainBalance, "ether") +' SMT</a>')
-            $("#SideChainTokenBalance").html(getMainChainWeb3().fromWei(r.SideChainTokenBalance, "ether")+ " EtherToken")
+            $("#SideChainTokenBalance").html('<a target="_blank" href="https://chain.smartmesh.io/token.html?tokenF='+sideChainContract+'&tokenT=' +myaccount+'">'+getMainChainWeb3().fromWei(r.SideChainTokenBalance, "ether") +' EtherToken</a>')
+            //https://chain.smartmesh.io/token.html?tokenF=0xbb4b3fa1448060e718540fbf7b74ed3d9f37d70e&tokenT=0x95af380b52d2e453b44a985682746f6600700d20
 
         }
         if (myaccount){
